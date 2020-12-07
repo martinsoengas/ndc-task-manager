1fa80b427806219c97a55d9796407872ae995c89
Martin Soengas <martinsoengas@Martins-Macbook-Pro.local>
2020-12-07 12:06:54 -0300

    added user auth and login

---
+++ src/index.js
@@ -0,0 +1,41 @@
+const express = require('express')
+require('./db/mongoose')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
+
+const app = express()
+const port = process.env.PORT || 3000
+
+app.use(express.json())
+app.use(userRouter)
+app.use(taskRouter)
+
+app.listen(port, () => {
+    console.log('Server is up on port ' + port)
+})
+
+// const bcrypt = require('bcryptjs')
+
+// const myFunction = async () => {
+//     const password = 'red12345!'
+//     const hashedPassword = await bcrypt.hash(password, 8)
+
+//     console.log(password)
+//     console.log(hashedPassword)
+
+//     const isMatch = await bcrypt.compare('red12345!', hashedPassword)
+//     console.log(isMatch)
+// }
+
+const jwt = require('jsonwebtoken')
+
+const myFunction = async () => {
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismysecret', { expiresIn: '7 days' })
+    console.log(token)
+
+    const data = jwt.verify(token, 'thisismysecret')
+    console.log(data)
+}
+
+myFunction()
+
