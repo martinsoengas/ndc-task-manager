c1842d3299c1aa16a3ff0abad3247eca4405a572
Martin Soengas <martinsoengas@Martins-Macbook-Pro.local>
2020-12-16 22:51:30 -0300

    added user-task relations

--- src/models/user.js
+++ src/models/user.js
@@ -49,6 +49,12 @@ const userSchema = new mongoose.Schema({
     }]
 })
 
+userSchema.virtual('tasks', {
+    ref: 'Task',
+    localField: '_id',
+    foreignField: 'owner'
+})
+
 const jwt = require('jsonwebtoken')
 
 userSchema.methods.toJSON = function () {
@@ -59,7 +65,6 @@ userSchema.methods.toJSON = function () {
     delete userObject.tokens
 
     return userObject
-
 }
 
 userSchema.methods.generateAuthToken = async function () {
