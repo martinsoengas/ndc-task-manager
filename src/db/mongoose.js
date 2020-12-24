4d51b7b7c9fb933214958aa881a3e3b59fa5f529
Martin Soengas <martinsoengas@Martins-Macbook-Pro.local>
2020-12-24 00:14:38 -0300

    added config

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.connect(process.env.MONGODB_URL, {
+    useMongoClient: true,
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false
